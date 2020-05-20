30d5cda61db3084e0d1c3a215975339f44b43acd
Yaroslav <snek44@gmail.com>
2020-05-20 21:36:32 +0400

    Добавлена модель базы данных

---
+++ Bars_TestWork/DataBaseModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bars_TestWork
+{
+    public class DataBaseModel
+    {
+        public string ServerName { get; set; }
+        public string DataBaseName { get; set; }
+        public double Size { get; set; }
+        public DateTime UpdateTime { get; set; }
+    }
+}

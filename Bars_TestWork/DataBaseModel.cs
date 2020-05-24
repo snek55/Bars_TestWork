a88f382eb347aff0cd46cfa7c9787162c9b4ce14
Yaroslav <snek44@gmail.com>
2020-05-24 16:53:22 +0400

    Изменены права доступа и добавлен метод общего вывода

--- Bars_TestWork/DataBaseModel.cs
+++ Bars_TestWork/DataBaseModel.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace Bars_TestWork
 {
     public class DataBaseModel
     {
-        public string ServerName { get; set; }
-        public string DataBaseName { get; set; }
-        public double Size { get; set; }
-        public DateTime UpdateTime { get; set; }
+        public string ServerName { private get; set; }
+        public string DataBaseName { private get; set; }
+        public double Size { private get; set; }
+        public DateTime UpdateTime { private get; set; }
+        public string DiscSize { get; set; }
+
+        public List<object> GetValues()
+        {
+            var values = new List<object>();
+
+            values.Add(ServerName);
+            values.Add(DataBaseName);
+            values.Add(Size);
+            values.Add(UpdateTime.ToString("d", CultureInfo.CurrentCulture));
+
+            return values;
+        }
     }
 }
